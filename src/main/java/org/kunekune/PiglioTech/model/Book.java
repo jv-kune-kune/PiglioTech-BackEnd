2179685b9738f72ca2ff74f2b91401f65ae65a1a
ge-drei <pjbutcher124@gmail.com>
2025-01-08 16:43:04 +0000

    model: add constructors, getters and setters to entities

--- src/main/java/org/kunekune/PiglioTech/model/Book.java
+++ src/main/java/org/kunekune/PiglioTech/model/Book.java
@@ -2,6 +2,7 @@ package org.kunekune.PiglioTech.model;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -21,4 +22,49 @@ public class Book {
 
     @ManyToMany(mappedBy = "books")
     private List<User> users;
+
+    public Book() {
+    }
+
+    public Book(String isbn, String author, Integer year, String thumbnail, String description) {
+        this.isbn = isbn;
+        this.author = author;
+        this.year = year;
+        this.thumbnail = thumbnail;
+        this.description = description;
+        this.users = new ArrayList<>();
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public Integer getYear() {
+        return year;
+    }
+    public String getThumbnail() {
+        return thumbnail;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public List<User> getUsers() {
+        return users;
+    }
+
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
--- src/main/java/org/kunekune/PiglioTech/model/User.java
+++ src/main/java/org/kunekune/PiglioTech/model/User.java
@@ -2,6 +2,7 @@ package org.kunekune.PiglioTech.model;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -26,4 +27,48 @@ public class User {
             inverseJoinColumns = @JoinColumn(name = "books_isbn")
     )
     private List<Book> books;
+
+    public User() {
+    }
+
+    public User(String uid, String name, String email, String phoneNumber, Region region) {
+        this.uid = uid;
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.region = region;
+        this.books = new ArrayList<>();
+    }
+
+    public String getUid() {
+        return uid;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public Region getRegion() {
+        return region;
+    }
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public void setRegion(Region region) {
+        this.region = region;
+    }
 }
