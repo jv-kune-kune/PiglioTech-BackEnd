e6713b08cae8597750e13e7ab60b63dbae03f109
ge-drei <pjbutcher124@gmail.com>
2025-01-09 15:58:51 +0000

    rearranged GET and POST endpoints, changed request mapping, added validation to POST endpoint

--- src/main/java/org/kunekune/PiglioTech/controller/UserController.java
+++ src/main/java/org/kunekune/PiglioTech/controller/UserController.java
@@ -3,33 +3,33 @@ package org.kunekune.PiglioTech.controller;
 import org.kunekune.PiglioTech.model.User;
 import org.kunekune.PiglioTech.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
-
 @RestController
-@RequestMapping("/api/user")  // base URL for user-related endpoints
-
+@RequestMapping("/api/v1/users")  // base URL for user-related endpoints
 public class UserController {
+
     @Autowired
-
     private UserService userService;
-
-    // save a new user
-    @PostMapping
-    public ResponseEntity<User> saveUser(@RequestBody User user) {
-        User savedUser = userService.saveUser(user);
-        return ResponseEntity.ok(savedUser);
-    }
 
     // get a user by ID
     @GetMapping("/{id}")
     public ResponseEntity<User> getUserById(@PathVariable String id) {
-       User user = userService.getUserByUid(id);
-       return ResponseEntity.status(404).body(null);
-
-        }
+        User user = userService.getUserByUid(id);
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
+
+    // save a new user
+    @PostMapping
+    public ResponseEntity<User> saveUser(@RequestBody User user) {
+        if (user.getUid() == null || user.getName() == null || user.getEmail() == null || user.getRegion() == null || user.getThumbnail() == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        User savedUser = userService.saveUser(user);
+        return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+    }
+}
 
 
