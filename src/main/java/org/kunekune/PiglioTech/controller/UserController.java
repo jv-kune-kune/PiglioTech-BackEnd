2f19b199cd082ade9f28226c12229cfc5967031e
kadri-k <kklampe@gmail.com>
2025-01-09 15:08:17 +0000

    controller: implemented saveUser and getUserById endpoints

--- src/main/java/org/kunekune/PiglioTech/controller/UserController.java
+++ src/main/java/org/kunekune/PiglioTech/controller/UserController.java
@@ -1,4 +1,35 @@
 package org.kunekune.PiglioTech.controller;
 
+import org.kunekune.PiglioTech.model.User;
+import org.kunekune.PiglioTech.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/user")  // base URL for user-related endpoints
+
 public class UserController {
-}
+    @Autowired
+
+    private UserService userService;
+
+    // save a new user
+    @PostMapping
+    public ResponseEntity<User> saveUser(@RequestBody User user) {
+        User savedUser = userService.saveUser(user);
+        return ResponseEntity.ok(savedUser);
+    }
+
+    // get a user by ID
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable String id) {
+       User user = userService.getUserByUid(id);
+       return ResponseEntity.status(404).body(null);
+
+        }
+    }
+
+
