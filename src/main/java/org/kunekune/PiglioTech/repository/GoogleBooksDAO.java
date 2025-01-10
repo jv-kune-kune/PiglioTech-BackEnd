b706fb82ab8477b1b4f40f858e16a6e6f5aad71b
ge-drei <pjbutcher124@gmail.com>
2025-01-10 13:27:21 +0000

    feat: add initial retrieval method to Google Books DAO

--- src/main/java/org/kunekune/PiglioTech/repository/GoogleBooksDAO.java
+++ src/main/java/org/kunekune/PiglioTech/repository/GoogleBooksDAO.java
@@ -1,15 +1,28 @@
-package org.kunekune.PiglioTech.service;
+package org.kunekune.PiglioTech.repository;
 
 import org.kunekune.PiglioTech.model.Book;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
-@Component
+@Repository
 public class GoogleBooksDAO {
 
-    public Mono<Book> fetchBookByIsbn(String isbn) {
-        // return empty - test
-        return Mono.empty();
+    private WebClient client;
 
+    public GoogleBooksDAO(String url) {
+        client = WebClient.create(url);
+    }
+
+    public GoogleBooksDAO() {
+        client = WebClient.create("https://www.googleapis.com/books/v1");
+    }
+
+    public Mono<Book> fetchBookByIsbn(String isbn) {
+        return client.get().uri(uriBuilder -> uriBuilder
+                        .path("/volumes")
+                        .queryParam("q", "isbn:" + isbn)
+                        .build())
+                .retrieve().bodyToMono(Book.class);
     }
 }
