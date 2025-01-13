db126493d4ea5b649f26f9b437c141122419b4cb
ge-drei <pjbutcher124@gmail.com>
2025-01-13 12:21:48 +0000

    feat: implement retrieving users by region in the service layer

--- src/main/java/org/kunekune/PiglioTech/repository/UserRepository.java
+++ src/main/java/org/kunekune/PiglioTech/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package org.kunekune.PiglioTech.repository;
 
+import org.kunekune.PiglioTech.model.Region;
 import org.springframework.data.repository.CrudRepository;
 import org.kunekune.PiglioTech.model.User;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends CrudRepository<User, String> {
+    List<User> getUsersByRegion(Region region);
 }
