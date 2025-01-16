cb8250039b936c8d14157a4b34d1048b14b3df17
kadri-k <kklampe@gmail.com>
2025-01-16 20:01:00 +0000

    feat: add validation for SwapRequest object

--- src/main/java/org/kunekune/PiglioTech/model/SwapRequestDto.java
+++ src/main/java/org/kunekune/PiglioTech/model/SwapRequestDto.java
@@ -1,4 +1,10 @@
 package org.kunekune.PiglioTech.model;
 
-public record SwapRequestDto(String initiatorUid, String receiverUid, String receiverIsbn) {
+import jakarta.validation.constraints.NotBlank;
+
+public record SwapRequestDto(
+        @NotBlank(message = "Initiator UID cannot be blank") String initiatorUid,
+        @NotBlank(message = "Receiver UID cannot be blank") String receiverUid,
+        @NotBlank(message = "Receiver Book ISBN cannot be blank") String receiverIsbn)
+{
 }
