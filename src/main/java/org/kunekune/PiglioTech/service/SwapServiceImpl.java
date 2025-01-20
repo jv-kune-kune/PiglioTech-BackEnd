5229149949b64083e2af34c24489240d93406a87
kadri-k <kklampe@gmail.com>
2025-01-20 11:54:40 +0000

    feat: refactor getMatches and makeSwapRequest

--- src/main/java/org/kunekune/PiglioTech/service/SwapServiceImpl.java
+++ src/main/java/org/kunekune/PiglioTech/service/SwapServiceImpl.java
@@ -37,65 +37,70 @@ public class SwapServiceImpl implements SwapService {
 
   @Override
   public SwapRequest makeSwapRequest(SwapRequestDto dto) {
-    List<SwapRequest> existingSwaps =
-        swapRequestRepository.findSwapRequestsByUid(dto.initiatorUid());
-    for (SwapRequest swap : existingSwaps) {
-      if (swap.getInitiator().getUid().equals(dto.initiatorUid())
-          && swap.getReceiver().getUid().equals(dto.receiverUid())
-          && swap.getReceiverBook().getIsbn().equals(dto.receiverIsbn())) {
-        throw new EntityExistsException("Identical swap request has already been made");
-      }
-
-      if (swap.getInitiator().getUid().equals(dto.receiverUid())
-          && swap.getReceiver().getUid().equals(dto.initiatorUid())) {
-        Match match = new Match(swap.getReceiver(), swap.getInitiator(), swap.getReceiverBook(),
-            swap.getInitiator().getBookByIsbn(dto.receiverIsbn()));
-        matchRepository.save(match);
-      }
+    List<Match> existingMatches = matchRepository.findMatchesBetweenUsers(dto.initiatorUid(), dto.receiverUid());
+    if (!existingMatches.isEmpty()) {
+      throw new EntityExistsException("Match already exists between users");
     }
 
-    User initiator = userRepository.findById(dto.initiatorUid()).orElseThrow();
-    User receiver = userRepository.findById(dto.receiverUid()).orElseThrow();
-    Book receieverBook = receiver.getBookByIsbn(dto.receiverIsbn());
+      List<SwapRequest> existingSwaps = swapRequestRepository.findSwapRequestsByUid(dto.initiatorUid());
+      for (SwapRequest swap : existingSwaps) {
+        if (swap.getInitiator().getUid().equals(dto.initiatorUid())
+                && swap.getReceiver().getUid().equals(dto.receiverUid())
+                && swap.getReceiverBook().getIsbn().equals(dto.receiverIsbn())) {
+          throw new EntityExistsException("Identical swap request has already been made");
+        }
 
-    SwapRequest request = new SwapRequest(initiator, receiver, receieverBook);
-
-    return swapRequestRepository.save(request);
-  }
-
-  @Override
-  public void dismissSwap(SwapDismissal dismissal) {
-    Match match = matchRepository.findById(dismissal.MatchId()).orElseThrow();
-
-    if (match.getUserOne().getUid().equals(dismissal.userId())) {
-      if (match.isUserOneDismissed())
-        return;
-      match.setUserOneDismissed(true);
-    } else if (match.getUserTwo().getUid().equals(dismissal.userId())) {
-      if (match.isUserTwoDismissed())
-        return;
-      match.setUserTwoDismissed(true);
-    } else {
-      throw new NoSuchElementException("No such match with user ID " + dismissal.userId());
-    }
-
-    if (match.isUserOneDismissed() && match.isUserTwoDismissed()) {
-      List<SwapRequest> swapRequests = swapRequestRepository
-          .findSwapRequestsBetweenUsers(match.getUserOne().getUid(), match.getUserTwo().getUid());
-      for (SwapRequest swap : swapRequests) {
-        if (swap.getReceiver().equals(match.getUserOne())
-            && swap.getReceiverBook().equals(match.getUserOneBook())
-            || swap.getReceiver().equals(match.getUserTwo())
-                && swap.getReceiverBook().equals(match.getUserTwoBook())) {
-          swapRequestRepository.delete(swap);
+        if (swap.getInitiator().getUid().equals(dto.receiverUid())
+                && swap.getReceiver().getUid().equals(dto.initiatorUid())) {
+          Match match = new Match(swap.getReceiver(), swap.getInitiator(), swap.getReceiverBook(),
+                  swap.getInitiator().getBookByIsbn(dto.receiverIsbn()));
+          matchRepository.save(match);
         }
       }
-      matchRepository.delete(match);
-      return;
+
+      User initiator = userRepository.findById(dto.initiatorUid()).orElseThrow();
+      User receiver = userRepository.findById(dto.receiverUid()).orElseThrow();
+      Book receieverBook = receiver.getBookByIsbn(dto.receiverIsbn());
+
+      SwapRequest request = new SwapRequest(initiator, receiver, receieverBook);
+
+      return swapRequestRepository.save(request);
     }
 
-    matchRepository.save(match);
+    @Override
+    public void dismissSwap (SwapDismissal dismissal){
+      Match match = matchRepository.findById(dismissal.MatchId()).orElseThrow();
+
+      if (match.getUserOne().getUid().equals(dismissal.userId())) {
+        if (match.isUserOneDismissed())
+          return;
+        match.setUserOneDismissed(true);
+      } else if (match.getUserTwo().getUid().equals(dismissal.userId())) {
+        if (match.isUserTwoDismissed())
+          return;
+        match.setUserTwoDismissed(true);
+      } else {
+        throw new NoSuchElementException("No such match with user ID " + dismissal.userId());
+      }
+
+      if (match.isUserOneDismissed() && match.isUserTwoDismissed()) {
+        List<SwapRequest> swapRequests = swapRequestRepository
+                .findSwapRequestsBetweenUsers(match.getUserOne().getUid(), match.getUserTwo().getUid());
+        for (SwapRequest swap : swapRequests) {
+          if (swap.getReceiver().equals(match.getUserOne())
+                  && swap.getReceiverBook().equals(match.getUserOneBook())
+                  || swap.getReceiver().equals(match.getUserTwo())
+                  && swap.getReceiverBook().equals(match.getUserTwoBook())) {
+            swapRequestRepository.delete(swap);
+          }
+        }
+        matchRepository.delete(match);
+        return;
+      }
+
+      matchRepository.save(match);
+    }
+
   }
-}
 
 
